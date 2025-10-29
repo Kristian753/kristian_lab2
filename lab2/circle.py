a926209214cacf071255327b9aa2aee1a1fe83c3
Kristian Tawfeeq <kristiantawfeeq@MacBook-Pro-som-tillhor-Kristian.local>
2025-10-29 14:38:38 +0100

    lab2 uppdatering

--- lab2/circle.py
+++ lab2/circle.py
@@ -1,7 +1,12 @@
 class Circle:
     def __init__(self, x, y, radius):
-        if Type(radius) not in [int, float]:
+        # kollar om radius är ett tal
+        if Type(radius) not in [int, float]: 
             raise TypeError("Radius must be a number.")
         self.x = x
         self.y = y
         self.radius = radius
+
+@property
+def area(self):
+    return 3.14 * self.radius * self.radius
