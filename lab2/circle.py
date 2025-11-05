741e9122caac11140065b46662c6b20e779d2daf
Kristian Tawfeeq <kristiantawfeeq@MacBook-Pro-som-tillhor-Kristian.local>
2025-11-05 09:22:45 +0100

    lab2 uppdatering

--- lab2/circle.py
+++ lab2/circle.py
@@ -1,31 +1,64 @@
 class Circle:
     def __init__(self, x, y, radius):
         # kollar om radius är ett tal
-        if Type(radius) not in [int, float]: 
+        if type(radius) not in [int, float]: 
             raise TypeError("Radius must be a number.")
+        if radius <= 0:
+            raise ValueError("Radius must be positive.")
         self.x = x
         self.y = y
         self.radius = radius
 
-@property
-def area(self):
+    @property
+    def area(self):
     # räkna area med pi
-    return 3.14 * self.radius * self.radius
+        return 3.14 * self.radius * self.radius
 
-@property
-def perimeter(self):
+    @property
+    def perimeter(self):
     # räknar cirkelns omkrets
-    return 2 * 3.14 * self.radius 
+        return 2 * 3.14 * self.radius 
 
-def translate(self, dx, dy):
+    def translate(self, dx, dy):
     # kollar om dx och dy är nummer
-    if type(dx) not in [int, float] or type(dy) not in [int, float]:
-        raise TypeError("dx and dy must be numbers")
-    self.x += dx
-    self.y += dy 
+        if type(dx) not in [int, float] or type(dy) not in [int, float]:
+            raise TypeError("dx and dy must be numbers")
+        self.x += dx
+        self.y += dy 
 
-def is_unit_circle(self):
+    def is_unit_circle(self):
     # kollar om cirkeln är en enhetscirkel
-    return self.radius == 1 and self.x == 0 and self.y == 0
+     return self.radius == 1 and self.x == 0 and self.y == 0
+
+    def __eq__(self, other):
+        if not isinstance(other, Circle): return False
+        return self.radius == other.radius
+    
+    def __gt__(self,other):
+        if not isinstance(other, Circle): return False
+        return self.radius > other
+    
+    def __lt__(self,other):
+        if not isinstance(other, Circle): return False
+        return self.radius < other.radius
+    
+    def __le__(self,other):
+        if not isinstance(other, Circle): return False
+        return self.radius <= other.radius
+    
+    def __ge__(self,other):
+        if not isinstance(other, Circle): return False
+        return self.radius >= other.radius
+    
+    def __repr__(self):
+        return f"Circle({self.x}, {self.y}, {self.radius})"
+    
+    def __str__(self):
+        return f"Circle with center at ({self.x}, {self.y}) and radius {self.radius}"
+    
+    
+    
+
+
  
 
