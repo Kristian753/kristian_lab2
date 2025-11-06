557005e5b1b843002f39ee915baa1563e81d42a1
Kristian Tawfeeq <kristiantawfeeq@Mac.lan>
2025-11-06 19:35:09 +0100

    lab2 rectangle början

--- lab2/rectangel.py
+++ lab2/rectangel.py
@@ -1,58 +1,18 @@
 class rectangel:
     def __init__(self,x,y,width, height):
+        # Kontrollerar att width är ett nummer
         if type(width) not in [int, float]: 
             raise TypeError("Width must be a number.")
+        # Kontrollerar att height är ett nummer
         if type(height) not in [int, float]: 
             raise TypeError("Height must be a number.")
+        # Kontrollerar att width är positivt
         if width <= 0:
             raise ValueError("Width must be positive.")
+        # Kontrollerar att height är positivt
         if height <= 0:
             raise ValueError("Height must be positive.")
         self.width = width 
         self.y = y
         self.x = x
         self.height = height
-
-    @property # read only property
-    def area(self):
-        return self.width * self.height
-    
-    @property # read only property
-    def perimeter(self):
-        return 2 * (self.width + self.height)
-    
-
-    def translate(self, dx, dy):
-        if type(dx) not in [int, float] or type(dy) not in [int, float]:
-            raise TypeError("dx and dy must be numbers")
-        self.x += dx
-        self.y += dy
-
-    def __eq__(self, other):
-        if not isinstance(other, rectangel): return False
-        return self.width == other.width and self.height == other.height
-    
-    def __gt__(self,other):
-        if not isinstance(other, rectangel): return False
-        return self.width * self.height > other.width * other.height
-    
-    def __lt__(self,other):
-        if not isinstance(other, rectangel): return False
-        return self.width * self.height < other.width * other.height
-    
-    def __le__(self,other):
-        if not isinstance(other, rectangel): return False
-        return self.width * self.height <= other.width * other.height
-    
-    def __ge__(self,other):
-        if not isinstance(other, rectangel): return False
-        return self.width * self.height >= other.width * other.height
-    
-    def __repr__(self):
-        return f"rectangel({self.x}, {self.y}, {self.width}, {self.height})"
-    
-    def __str__(self):
-        return f"rectangel with top-left corner at ({self.x}, {self.y}), width {self.width} and height {self.height}"
-    
-    def is_square(self):
-        return self.width == self.height
