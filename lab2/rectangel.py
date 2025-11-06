1263574063165586753e076271337ae787d1ef67
Kristian Tawfeeq <kristiantawfeeq@Mac.lan>
2025-11-06 23:38:05 +0100

    lab2 update rectangle

--- lab2/rectangel.py
+++ lab2/rectangel.py
@@ -16,3 +16,28 @@ class rectangel:
         self.y = y
         self.x = x
         self.height = height
+
+    @property # read only property
+    def area(self):
+        # Beräknar rektangelns area
+        return self.width * self.height
+    
+    @property # read only property
+    def perimeter(self):
+        # Beräknar rektangelns omkrets
+        return 2 * (self.width + self.height)
+    
+
+    def translate(self, dx, dy):
+        # Kontrollerar att dx och dy är nummer
+        if type(dx) not in [int, float] or type(dy) not in [int, float]:
+            raise TypeError("dx and dy must be numbers")
+        # Flyttar rektangelns x-position
+        self.x += dx
+        # Flyttar rektangelns y-position
+        self.y += dy
+
+    def __eq__(self, other):
+        # Jämför om två rektanglar har samma mått
+        if not isinstance(other, rectangel): return False
+        return self.width == other.width and self.height == other.height
